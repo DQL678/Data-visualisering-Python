c41472df2fda11ba29be91e3c508a59d409b9825
ChristianProla <christian.prola@icloud.com>
2026-01-09 13:11:35 +0100

    Rigtigt navngivet forside (Engelsk)

--- StartMenu.py
+++ StartMenu.py
@@ -6,19 +6,19 @@ root.title("Startmenu")
 root.geometry("300x200")
 
 # Overskrift oprettes
-overskrift = tk.Label(root, text="Velkommen til valuta-visualisering App", font=("Arial", 12))
+overskrift = tk.Label(root, text="Welcome to the currency visualization app", font=("Arial", 12))
 overskrift.pack(pady=10)
 
 # Knap 1
-knap1 = tk.Button(root, text="Valuta Information")
+knap1 = tk.Button(root, text="Currency Information")
 knap1.pack(pady=5)
 
 # Knap 2
-knap2 = tk.Button(root, text="Valuta Sammenligning")
+knap2 = tk.Button(root, text="Currency Comparison")
 knap2.pack(pady=5)
 
 # Knap 3
-knap3 = tk.Button(root, text="Valuta Visualisering")
+knap3 = tk.Button(root, text="Currency Visualization")
 knap3.pack(pady=5)
 
 # Dette kører vinduet

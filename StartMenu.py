8b03f32ad21830d4c9c0e1d5c241980dc84761ed
ChristianProla <christian.prola@icloud.com>
2026-01-09 12:30:12 +0100

    Den første startmenu

---
+++ StartMenu.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+# Vinduet oprettes
+root = tk.Tk()
+root.title("Startmenu")
+root.geometry("300x200")
+
+# Overskrift oprettes
+overskrift = tk.Label(root, text="Velkommen til Fodboldtur App", font=("Arial", 12))
+overskrift.pack(pady=10)
+
+# Knap 1
+knap1 = tk.Button(root, text="Vindue 1")
+knap1.pack(pady=5)
+
+# Knap 2
+knap2 = tk.Button(root, text="Vindue 2")
+knap2.pack(pady=5)
+
+# Knap 3
+knap3 = tk.Button(root, text="Vindue 3")
+knap3.pack(pady=5)
+
+# Dette kører vinduet
+root.mainloop()

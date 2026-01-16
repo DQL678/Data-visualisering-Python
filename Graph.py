530b71ff9b20c61f1c46b711c870ff8a034febe6
DQL678 <dungql678@gmail.com>
2026-01-16 14:03:40 +0100

    Laver input til base valuta og udskrift af alle valutaer

--- Graph.py
+++ Graph.py
@@ -25,9 +25,19 @@ def get_data(valutas, base):
     data = response.json()
     return format_json(data)
 
+# Liste of alle valutaer
+def get_supported_currencies():
+    url2 = "https://api.frankfurter.dev/v1/currencies"
+    response = requests.get(url2)
+    return response.json()
+
+currencies = get_supported_currencies()
+for code, name in currencies.items():
+    print(f"{code}: {name}")
+# Slut liste
 
 valutas = ["USD", "DKK", "GBP"]
-base = "AUD"
+base = input("Indtast basisvaluta (fx EUR, USD, AUD): ").upper()
 data = get_data(valutas, base)
 
 # Lidt info
@@ -52,3 +62,5 @@ plt.ylabel("Kurs", fontsize = 12)
 plt.legend()
 plt.grid(True)
 plt.show()
+
+
